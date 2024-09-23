93a4e89d297d193d4e9a29e00e2360b77f5fc718
Nicholas Romano <njr7romano@yahoo.com>
2024-09-23 14:35:10 -0400

    Fixed search input bug

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="bg-text-sm focus:ring-yello-500 w-28 rounded-full px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+        className="text-black bg-text-sm focus:ring-yellow-500 w-28 rounded-full px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
